69f3c30e72a06d2f95f17f8f62c835af4b853516
Sherika Parfitt <scparfitt@email.neit.edu>
2014-08-31 15:30:40 -0700

    fixer1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,17 +5,17 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit root_path
+      visit 'static_pages/home'
       expect(page).to have_content('Sample App')
     end
 
     it "should have the base title" do
-      visit root_path
+      visit 'static_pages/home'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
-      visit root_path
+      visit 'static_pages/home'
       expect(page).not_to have_title('| Home')
     end
   end
@@ -23,12 +23,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit help_path
+      visit 'static_pages/help'
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit help_path
+      visit 'static_pages/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
@@ -36,12 +36,12 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit about_path
+      visit 'static_pages/about'
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit about_path
+      visit 'static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
@@ -49,12 +49,12 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit contact_path
+      visit 'static_pages/contact'
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit contact_path
+      visit 'static_pages/contact'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
