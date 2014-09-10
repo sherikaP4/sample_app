3b73b0f6cb8666b20775b9f0f03e8ec5ffce2f36
Sherika Parfitt <scparfitt@email.neit.edu>
2014-09-09 17:14:03 -0700

    Finish layout and routes with exercise

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,27 +2,30 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  subject{page}
+  subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "Home page" do
-    before{visit root_path}
+    before { visit root_path }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
-    it {should have_content ('Sample App')}
-
-    it {should have_title (full_title(''))}
-
-    it {should_not have_title ('| Home')}
-    end
-
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
+  end
 
   describe "Help page" do
-    before{visit help_path}
+    before { visit help_path }
 
-    it {should have_content ('Help')}
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it {should have_title (full_title('Help'))}
-
-    end
+    it_should_behave_like "all static pages"
+  end
 
 
   describe "About page" do
@@ -38,10 +41,24 @@ describe "Static pages" do
   describe "Contact page" do
     before{visit contact_path}
 
-    it {should have_content ('Contact')}
+    it {should have_selector ('h1', text: 'Contact')}
 
     it {should have_title (full_title('Contact'))}
 
+end
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      expect(page).to have_title(full_title('About Us'))
+      click_link "Help"
+      expect(page).to help
+      click_link "Contact"
+      expect(page).to contact
+      click_link "Home"
+      click_link "Sign up now!"
+      expect(page).to signup
+      click_link "sample app"
+      expect(page).to home
 
   end
 end
