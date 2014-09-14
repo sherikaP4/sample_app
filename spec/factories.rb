f70f635d0ed8a7c5046881eda41240465184e438
Sherika Parfitt <scparfitt@email.neit.edu>
2014-09-14 14:20:34 -0700

    update

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Sherika Parfitt"
+    email    "scparfitt@email.neit.edu"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
