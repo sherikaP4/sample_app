6c0610cea44850ea707490e4923c82b715ff7588
Sherika Parfitt <scparfitt@email.neit.edu>
2014-09-18 08:48:37 -0700

    up to listing 9.45

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,13 @@ class UsersController < ApplicationController
     end
   end
 
+ def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
+
   private
 
   def user_params
