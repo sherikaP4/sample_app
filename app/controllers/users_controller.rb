4d45139ffd41ad539d9fcf34c995b8466df2b687
Sherika Parfitt <scparfitt@email.neit.edu>
2014-09-18 20:40:48 -0700

    updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+
   end
 
   def new
