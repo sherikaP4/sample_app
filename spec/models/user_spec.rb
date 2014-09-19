1f2940a5454ea628d022ff7dbfb76ab2e430bb58
Sherika Parfitt <scparfitt@email.neit.edu>
2014-09-19 06:07:30 -0700

    up

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,8 +15,9 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
- it { should respond_to(:admin) }
+  it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
 
 
@@ -144,6 +145,16 @@ it { should_not be_admin }
         expect(Micropost.where(id: micropost.id)).to be_empty
       end
 
+    end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
     end
 
   end
